20f42bdcaacca968ec580408541632ed5329ed88
jshenouda <shenoudajoseph7@gmail.com>
2024-03-14 20:15:05 -0500

    fixed ssim bug and refactored files in modules

--- exp_superres.py
+++ exp_superres.py
@@ -41,8 +41,9 @@ if __name__ == '__main__':
     parser.add_argument('--image', type=str, default='butterfly', help='Which pic [butterfly, rhino, nemo]')
 
     # Model arguments
-    parser.add_argument('--omega0', type=float, default=8.0, help='Global scaling for activation')
-    parser.add_argument('--sigma0', type=float, default=6.0, help='Global scaling for activation')
+    parser.add_argument('--omega0', type=float, default=8.0, help='Global scaling for SIREN, WIRE activation')
+    parser.add_argument('--sigma0', type=float, default=6.0, help='Global scaling for WIRE activation')
+    parser.add_argument('--c', type=float, default=1.0, help='Global scaling for BW-ReLU activation')
     parser.add_argument('--layers', type=int, default=2, help='Number of hidden layers')
     parser.add_argument('--width', type=int, default=256, help='Width for layers of MLP')
     parser.add_argument('--init-scale', type=float, default=1, help='Initial scaling to apply to weights')
@@ -55,12 +56,9 @@ if __name__ == '__main__':
     parser.add_argument('--lam', type=float, default=0, help='Weight decay/Path Norm param')
     parser.add_argument('--path-norm', action=argparse.BooleanOptionalAction)
     parser.add_argument('--weight-norm', action='store_true', help='Use weight normalization')
-    parser.add_argument('--holdout', type=float, default=0, help='Number of points to use for holdout')
     parser.add_argument('--device', type=int, default=0, help='GPU to use')
     parser.add_argument('--rand-seed', type=int, default=40)
 
-    
-    
     # Parse the arguments
     args = parser.parse_args()
     
@@ -79,14 +77,16 @@ if __name__ == '__main__':
     # If you want the original values for omega0 and sigma0 use default of the args
     omega0 = args.omega0          # Frequency of sinusoid
     sigma0 = args.sigma0          # Sigma of Gaussian
+    c = args.c                    # Scaling for BW-ReLU
     
     # Network parameters
     hidden_layers = args.layers    # Number of hidden layers in the MLP
     hidden_features = args.width   # Number of hidden units per layer
     device = 'cuda:{}'.format(args.device)
 
-    save_dir = 'results/sisr/{}_SR_img_scale_{}_omega_{}_sigma_{}_lr_{}_lam_{}_PN_{}_width_{}_layers_{}_lin_{}_epochs_{}_{}'.format(nonlin,
-                                                                                                        scale_im, 
+    save_dir = 'results/sisr/{}_SR_img_scale_{}_c_{}_omega_{}_sigma_{}_lr_{}_lam_{}_PN_{}_width_{}_layers_{}_lin_{}_epochs_{}_{}'.format(nonlin,
+                                                                                                        scale_im,
+                                                                                                        c,
                                                                                                         omega0,
                                                                                                         sigma0,
                                                                                                         learning_rate, 
@@ -135,6 +135,7 @@ if __name__ == '__main__':
                     hidden_layers=hidden_layers,
                     first_omega_0=omega0,
                     hidden_omega_0=omega0,
+                    c=c,
                     scale=sigma0,
                     pos_encode=posencode,
                     sidelength=sidelength,
@@ -176,14 +177,12 @@ if __name__ == '__main__':
     im_gt = gt.reshape(H, W, 3).permute(2, 0, 1)[None, ...]
     im_bi_ten = torch.tensor(im_bi).to(device).permute(2, 0, 1)[None, ...]
     
-    print(utils.psnr(im, im_bi), ssim_func(im, im_bi, multichannel=True))
+    print(utils.psnr(im, im_bi), ssim_func(im, im_bi, channel_axis=2))
 
-    mse_hr_array = torch.zeros(niters, device=device) # high res mse (test error)
-    mse_lr_array = torch.zeros(niters, device=device) # low res mse  (train error)
-    mse_val_array = torch.zeros(niters, device=device)
-    ssim_array = torch.zeros(niters, device=device)
-    lpips_array = torch.zeros(niters, device=device)
-    
+    mse_hr_array = [] # high res mse (test error)
+    mse_lr_array = [] # low res mse  (train error)
+    ssim_array = []
+
     cond_num_iters = []
     cond_num_array = []
     path_norms_array = []
@@ -217,18 +216,22 @@ if __name__ == '__main__':
 
             path_norms_array.append(path_norm.item())
 
-
         loss = ((gt_lr - rec.reshape(1, 3, -1).permute(0, 2, 1))**2).mean()
+
         with torch.no_grad():
             rec_hr = model(coords_hr)
 
             im_rec = rec_hr.reshape(H, W, 3).permute(2, 0, 1)[None, ...]
 
-            mse_hr_array[epoch] = ((gt - rec_hr)**2).mean().item()
-            ssim_array[epoch] = ssim_func(im_gt, im_rec, data_range=1, size_average=True)
+            mse_hr_array.append(((gt - rec_hr)**2).mean().item())
+
+            # sssim_func only accepts numpy arrays
+            im_gt_np = im_gt.detach().cpu().numpy().squeeze().transpose(1, 2, 0)
+            im_rec_np = im_rec.detach().cpu().numpy().squeeze().transpose(1, 2, 0)
+            ssim_array.append(ssim_func(im_gt_np, im_rec_np, channel_axis=2))
 
 
-        mse_lr_array[epoch] = loss.item()
+        mse_lr_array.append(loss.item())
 
         if nonlin == 'bspline-w' and args.path_norm:
             tbar.set_description('{:2f}: Loss LR {:e}, Path Norm: {}'.format(-10*torch.log10(mse_hr_array[epoch]), loss.item(), path_norm.item()))
@@ -238,8 +241,8 @@ if __name__ == '__main__':
         tbar.refresh()
 
         if epoch % 10000 == 0:
-            np.save(os.path.join(save_dir, 'mse_hr_array_t_{}'.format(epoch)), mse_hr_array.cpu().numpy())
-            np.save(os.path.join(save_dir, 'mse_lr_array_t_{}'.format(epoch)), mse_lr_array.cpu().numpy())
+            np.save(os.path.join(save_dir, 'mse_hr_array_t_{}'.format(epoch)), mse_hr_array)
+            np.save(os.path.join(save_dir, 'mse_lr_array_t_{}'.format(epoch)), mse_lr_array)
             np.save(os.path.join(save_dir, 'path_norm_array_t_{}'.format(epoch)), path_norms_array)    
             torch.save(model.state_dict(), os.path.join(save_dir, 'model_t_{}.pt'.format(epoch)))
 
@@ -269,9 +272,9 @@ if __name__ == '__main__':
     if posencode:
         nonlin = 'posenc'
 
-    np.save(os.path.join(save_dir, 'ssim_array'), ssim_array.cpu().numpy())
-    np.save(os.path.join(save_dir, 'mse_hr_array'), mse_hr_array.cpu().numpy())
-    np.save(os.path.join(save_dir, 'mse_lr_array'), mse_lr_array.cpu().numpy())
+    np.save(os.path.join(save_dir, 'ssim_array'), ssim_array)
+    np.save(os.path.join(save_dir, 'mse_hr_array'), mse_hr_array)
+    np.save(os.path.join(save_dir, 'mse_lr_array'), mse_lr_array)
     np.save(os.path.join(save_dir, 'cond_num_arr'), cond_num_array)
     np.save(os.path.join(save_dir, 'cond_num_iters'), cond_num_iters)
     np.save(os.path.join(save_dir, 'path_norm_array'), path_norms_array)    
--- modules/bspline_w.py
+++ modules/bspline_w.py
@@ -15,12 +15,14 @@ import torch.nn.utils.weight_norm as w_norm
 
 @torch.jit.script
 def bspline_wavelet(x, scale):
-    return (1 / 6) * F.relu(scale*x) - (8 / 6) * F.relu(scale*x - (1 / 2)) + (23 / 6) * F.relu(scale*x - (1)) - (16 / 3) * F.relu(scale*x - (3 / 2)) + (23 / 6) * F.relu(scale*x - (2)) - (8 / 6) * F.relu(scale*x - (5 / 2)) +(1 / 6) * F.relu(scale*x - (3))
+    return (1 / 6) * F.relu(scale*x) - (8 / 6) * F.relu(scale*x - (1 / 2)) + (23 / 6) \
+        * F.relu(scale*x - (1)) - (16 / 3) * F.relu(scale*x - (3 / 2)) + (23 / 6) * F.relu(scale*x - (2)) - (8 / 6) \
+        * F.relu(scale*x - (5 / 2)) +(1 / 6) * F.relu(scale*x - (3))
 
 class BSplineWavelet(nn.Module):
     def __init__(self, scale=torch.as_tensor(1)):
         super().__init__()
-        self.scale = scale
+        self.scale = torch.as_tensor(scale)
     
     def forward(self, x):
         output = bspline_wavelet(x, self.scale)
@@ -94,11 +96,8 @@ class BsplineWaveletLayer(nn.Module):
             lin_layer_V.weight.data = init_scale * lin_layer_V.weight.data
             self.wavelon.append(('linear2',lin_layer_V))
 
-        self.block= torch.nn.Sequential(collections.OrderedDict(self.wavelon))
+        self.block = torch.nn.Sequential(collections.OrderedDict(self.wavelon))
 
-            
-    
-    
     def forward(self, input):
         out = self.block(input)
         
@@ -109,69 +108,56 @@ class INR(nn.Module):
                  hidden_features, 
                  hidden_layers, 
                  out_features,
-                 first_omega_0, 
-                 hidden_omega_0,
-                 scale, 
-                 pos_encode, 
-                 sidelength, 
-                 fn_samples, 
-                 use_nyquist, 
-                 outermost_linear=True, 
+                 c,
                  skip_conn=False, 
                  resnet=False, 
                  weight_norm=False, 
                  init_scale=1,
                  linear_layers=False,
-                 use_c=False):
+                 learn_c=False):
         
         super().__init__()
-        
-        if resnet:
-            self.nonline = BsplineWaveletResNet
-        else:
-            self.nonlin = BsplineWaveletLayer
+
+        self.nonlin = BsplineWaveletLayer
         
         self.wavelet = 'bspline'    
                     
         self.net = []
 
         if hidden_layers == 1:
-            if use_c:
-                first_omega_0 = nn.Parameter(torch.as_tensor(first_omega_0), requires_grad=True)
+            if learn_c:
+                c = nn.Parameter(torch.as_tensor(c), requires_grad=True)
             self.net.append(self.nonlin(in_features,
                                         hidden_features,
                                         weight_norm=weight_norm,  
-                                        c=first_omega_0, init_scale=init_scale,
+                                        c=c, init_scale=init_scale,
                                         linear_layer=False))
 
             # Final layer
             self.net.append(torch.nn.Linear(hidden_features, out_features))
-            
 
         if hidden_layers > 1:
-            if use_c:
-                hidden_omega_0 = nn.Parameter(torch.as_tensor(hidden_omega_0), requires_grad=True)
+            if learn_c:
+                c = nn.Parameter(torch.as_tensor(c), requires_grad=True)
             self.net.append(self.nonlin(in_features,
                                         hidden_features, weight_norm=weight_norm,
-                                        c=hidden_omega_0, 
+                                        c=c,
                                         init_scale=init_scale, linear_layer=linear_layers))
 
             for i in range(hidden_layers-2):
                 self.net.append(self.nonlin(hidden_features,
                                         hidden_features, weight_norm=weight_norm,
-                                        c=hidden_omega_0, 
+                                        c=c,
                                         init_scale=init_scale,  linear_layer=linear_layers))
             
             final_layer = self.nonlin(hidden_features,
                                         hidden_features, weight_norm=weight_norm,
-                                        c=hidden_omega_0, 
+                                        c=c,
                                         init_scale=init_scale,  linear_layer=False)
             
-            final_layer.block.add_module('linear2',nn.Linear(hidden_features, out_features))
+            final_layer.block.add_module('linear2', nn.Linear(hidden_features, out_features))
             self.net.append(final_layer)
 
-
-            
         self.net = nn.Sequential(*self.net)
         
         if skip_conn and not resnet:
@@ -179,12 +165,9 @@ class INR(nn.Module):
             self.skip_connection = nn.Linear(in_features, out_features)
         else:
             self.skip_conn = False
-        
-    
+
     def forward(self, coords):
         output = self.net(coords)
-        
         if self.skip_conn:
             output = output + self.skip_connection(coords)
-         
         return output
